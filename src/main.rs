31c55412e6c3b6ca63f9fe4a8ade105c07aeebbd
Vincent Cordobes <vincent.cordobes@gmail.com>
2021-04-11 22:06:31 +0200

    Implement spaced repetition

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,84 @@ use crossterm::{
 };
 use rand::distributions::{Distribution, Standard, Uniform};
 use rand::Rng;
+use std::collections::{BinaryHeap, HashSet};
 use std::fmt;
 use std::io::stdout;
 
-#[derive(Debug)]
+const PERIODS: &[u32] = &[1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
+
+#[derive(Hash, PartialEq, Eq, Clone, Copy)]
 struct Factors(u8, u8);
+
+#[derive(Hash, PartialEq, Eq, Clone)]
+struct Card {
+    factors: Factors,
+    period_i: usize,
+    period: u32,
+    last_tick: u32,
+}
+
+impl fmt::Debug for Card {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "Card ({:?}, next_tick {:?} last_tick {:?}, period: {:?})",
+            self.factors,
+            self.next_tick(),
+            self.last_tick,
+            self.period,
+        )
+    }
+}
+
+impl Card {
+    fn new(x: u8, y: u8) -> Card {
+        Card {
+            factors: Factors(x, y),
+            period_i: 0,
+            last_tick: 0,
+            period: 1,
+        }
+    }
+
+    fn next_tick(&self) -> u32 {
+        let period = PERIODS[self.period_i];
+        self.last_tick + period
+    }
+
+    fn graduates(&self) -> Card {
+        let max_i = PERIODS.len() - 1;
+        let period_i = std::cmp::min(self.period_i + 1, max_i);
+        let period = PERIODS[period_i];
+        Card {
+            period_i,
+            period,
+            ..*self
+        }
+    }
+
+    fn reset(&self) -> Card {
+        let period_i = 0;
+        let period = PERIODS[period_i];
+        Card {
+            period_i: 0,
+            period,
+            ..*self
+        }
+    }
+}
+
+impl Ord for Card {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.next_tick().cmp(&self.next_tick())
+    }
+}
+
+impl PartialOrd for Card {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 impl Factors {
     fn compute(&self) -> u8 {
@@ -31,6 +104,12 @@ impl fmt::Display for Factors {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let Factors(x, y) = self;
         write!(f, "{} x {}", x, y)
+    }
+}
+
+impl fmt::Debug for Factors {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Display::fmt(self, f)
     }
 }
 
@@ -54,6 +133,17 @@ enum UserInput {
     Abort,
 }
 
+fn validate_input(line: &String) -> Result<UserInput> {
+    if line.is_empty() {
+        Err(ErrorKind::EmptyInput)
+    } else {
+        match line.trim().parse() {
+            Ok(answer) => Ok(UserInput::Answer(answer)),
+            Err(err) => Err(ErrorKind::InvalidInputError(err)),
+        }
+    }
+}
+
 fn read_input() -> Result<UserInput> {
     let mut line = String::new();
 
@@ -68,16 +158,12 @@ fn read_input() -> Result<UserInput> {
             Event::Key(KeyEvent {
                 code: KeyCode::Enter,
                 ..
-            }) => {
-                if line.is_empty() {
-                    break Err(ErrorKind::EmptyInput);
-                } else {
-                    break match line.trim().parse() {
-                        Ok(answer) => Ok(UserInput::Answer(answer)),
-                        Err(err) => Err(ErrorKind::InvalidInputError(err)),
-                    };
-                }
-            }
+            }) => break validate_input(&line),
+
+            Event::Key(KeyEvent {
+                code: KeyCode::Char(' '),
+                ..
+            }) => break validate_input(&line),
 
             Event::Key(KeyEvent {
                 code: KeyCode::Backspace,
@@ -114,21 +200,66 @@ fn plural(x: usize) -> &'static str {
     }
 }
 
+fn generate_all_cards() -> Vec<Card> {
+    let mut set = Vec::new();
+    for x in 2..10 {
+        for y in 2..10 {
+            let card = Card::new(x, y);
+            set.push(card);
+        }
+    }
+    set
+}
+
 fn main() -> Result<()> {
+    env_logger::init();
+    // let mut db: Vec<&Card> = Vec::new();
+
+    let mut heap: BinaryHeap<Card> = BinaryHeap::new();
+
     let mut stdout = stdout();
     terminal::enable_raw_mode()?;
     execute!(stdout, terminal::EnterAlternateScreen)?;
 
-    let mut rng = rand::thread_rng();
+    let all_cards: Vec<_> = generate_all_cards();
+    // let seen_cards: HashSet<_> = heap.iter().cloned().collect();
+    // let mut unseen_cards: Vec<_> = all_cards.difference(&seen_cards).collect();
+    let mut unseen_cards: Vec<_> = all_cards.iter().cloned().collect();
+
+    log::debug!("{:?}", unseen_cards);
 
     let mut ok_count = 0;
     let mut ko_count = 0;
+    let mut tick = 0;
 
     loop {
-        let factors: Factors = rng.gen();
-        execute!(stdout, style::Print(format!("{} = ", &factors)))?;
+        let next_scheduled = heap.peek().cloned();
 
-        let product = factors.compute();
+        log::debug!("Current tick {}", tick);
+        log::debug!("heap {:?}", &heap);
+        log::debug!("{:?}", next_scheduled);
+
+        let card = match next_scheduled {
+            Some(card) if card.next_tick() <= tick => {
+                log::debug!("Card from heap {:?}", card,);
+                heap.pop();
+                Card {
+                    last_tick: tick,
+                    ..card
+                }
+            }
+            _ => {
+                log::debug!("New card from pool");
+                Card {
+                    last_tick: tick,
+                    ..unseen_cards.pop().unwrap()
+                }
+            }
+        };
+
+        execute!(stdout, style::Print(format!("{} = ", &card.factors)))?;
+
+        let product = card.factors.compute();
 
         let ok = match read_input() {
             Ok(UserInput::Answer(answer)) => answer == product,
@@ -145,23 +276,27 @@ fn main() -> Result<()> {
 
         if ok {
             ok_count += 1;
+            heap.push(card.graduates());
+
             execute!(
                 stdout,
-                style::Print(format!("{} = {}", &factors, product)),
+                style::Print(format!("{} = {}", &card.factors, product)),
                 style::SetForegroundColor(Color::Green),
                 style::Print(" OK"),
             )?;
         } else {
             ko_count += 1;
+            heap.push(card.reset());
             execute!(
                 stdout,
-                style::Print(format!("{} = {}", &factors, product)),
+                style::Print(format!("{} = {}", &card.factors, product)),
                 style::SetForegroundColor(Color::Red),
                 style::Print(" KO!!!"),
             )?;
         }
 
         execute!(stdout, style::ResetColor, cursor::MoveToNextLine(1))?;
+        tick += 1;
     }
 
     terminal::disable_raw_mode()?;
